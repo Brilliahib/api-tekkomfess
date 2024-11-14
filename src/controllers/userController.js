68cf24f50d009e567a5f1aa88dd9d6268db3dce7
brilliahib <brilliahib21@gmail.com>
2024-11-14 18:37:36 +0700

    feat: get all menfess by user id

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -83,7 +83,61 @@ const getUserByUid = async (req, res) => {
   }
 };
 
+// Get all menfess by a specific user ID (UID)
+const getMenfessByUserId = async (req, res) => {
+  const { uid } = req.params;
+
+  if (!uid) {
+    return res.status(400).json({
+      statusCode: 400,
+      message: "UID is required",
+    });
+  }
+
+  try {
+    const menfessCollection = collection(db, "menfess");
+    const q = query(menfessCollection, where("user.uid", "==", uid));
+    const menfessSnapshot = await getDocs(q);
+
+    if (menfessSnapshot.empty) {
+      return res.status(404).json({
+        statusCode: 404,
+        message: "No menfess found for this user",
+      });
+    }
+
+    const menfessList = menfessSnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        id: doc.id,
+        message: data.message,
+        images: data.images,
+        comments: data.comments,
+        created_at: data.created_at.toDate(),
+        user: {
+          uid: data.user.uid,
+          email: data.user.email,
+          fullname: data.user.fullname,
+        },
+      };
+    });
+
+    res.status(200).json({
+      statusCode: 200,
+      message: "Menfess retrieved successfully",
+      data: menfessList,
+    });
+  } catch (error) {
+    res.status(500).json({
+      statusCode: 500,
+      message: "Error retrieving menfess by user ID",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   getAllUsers,
   getUserByUid,
+  getMenfessByUserId,
 };
